a168142839f57137668b9c113a9e5d612ad3ff77
weifegn <1982392655@qq.com>
2024-12-26 11:54:15 +0800

    feat:新增超级公式

---
+++ Luckysheet-source/src/controllers/superFormula.js
@@ -0,0 +1,31 @@
+/**
+ * 超级公式相关实现逻辑
+ */
+
+function super_compute() {
+	console.log("==> super_compute",);
+}
+function super_dateDiff() {
+	console.log("==> super_dateDiff",);
+}
+function super_ranking() {
+	console.log("==> super_ranking",);
+}
+function super_idInfo() {
+	console.log("==> super_idInfo",);
+}
+function super_script() {
+	console.log("==> super_script",);
+}
+function super_latex() {
+	console.log("==> super_latex",);
+}
+
+export {
+	super_compute,
+	super_dateDiff,
+	super_ranking,
+	super_idInfo,
+	super_script,
+	super_latex,
+};
