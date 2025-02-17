458eafc7da4468d0c4c72bb5b2cf16f9fc5d67d9
weifegn <1982392655@qq.com>
2025-02-17 17:28:24 +0800

    fix:修复已知BUG

--- server/src/Service/Border.ts
+++ server/src/Service/Border.ts
@@ -1,52 +1,68 @@
 import {
-  BorderInfoModel,
-  BorderInfoModelType,
+	BorderInfoModel,
+	BorderInfoModelType,
 } from "../Sequelize/Models/BorderInfo";
 import { logger } from "../Utils/Logger";
 
-// 查找是否存在 range de边框
+/**
+ * 查找是否存在边框
+ * @param info
+ * @returns
+ */
 async function hasConfigBorder(info: BorderInfoModelType) {
-  try {
-    return await BorderInfoModel.findOne({
-      where: info,
-    });
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		return await BorderInfoModel.findOne({
+			where: info,
+		});
+	} catch (error) {
+		logger.error(error);
+	}
 }
 
-// 创建新的边框记录
+/**
+ * 创建新的边框记录
+ * @param info
+ * @returns
+ */
 async function createConfigBorder(info: BorderInfoModelType) {
-  try {
-    return await BorderInfoModel.create(info);
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		return await BorderInfoModel.create(info);
+	} catch (error) {
+		logger.error(error);
+	}
 }
 
-// 更新边框记录
+/**
+ * 更新边框记录
+ * @param info
+ * @returns
+ */
 async function updateConfigBorder(info: BorderInfoModelType) {
-  try {
-    return await BorderInfoModel.update(info, {
-      where: { config_border_id: info.config_border_id },
-    });
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		return await BorderInfoModel.update(info, {
+			where: { config_border_id: info.config_border_id },
+		});
+	} catch (error) {
+		logger.error(error);
+	}
 }
 
-// 初始化查询全部
+/**
+ * 初始化查询全部 borderInfo
+ * @param worker_sheet_id
+ * @returns
+ */
 async function findAll(worker_sheet_id: string) {
-  try {
-    return await BorderInfoModel.findAll({ where: { worker_sheet_id } });
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		return await BorderInfoModel.findAll({ where: { worker_sheet_id } });
+	} catch (error) {
+		logger.error(error);
+	}
 }
 
 export const BorderInfoService = {
-  hasConfigBorder,
-  createConfigBorder,
-  updateConfigBorder,
-  findAll,
+	findAll,
+	hasConfigBorder,
+	createConfigBorder,
+	updateConfigBorder,
 };
