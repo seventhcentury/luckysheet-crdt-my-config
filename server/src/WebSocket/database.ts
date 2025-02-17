7169345547d197c39a88b5199d2e281d4dbbcb8a
weifegn <1982392655@qq.com>
2025-02-17 16:07:39 +0800

    fix:修复范围单元格操作时，delete 删除后数据库记录仍存在的BUG

--- server/src/WebSocket/database.ts
+++ server/src/WebSocket/database.ts
@@ -180,12 +180,25 @@ async function v(data: string) {
 	}
 }
 
-// 范围单元格刷新
+/**
+ * 范围单元格刷新 - 与 单个单元格刷新的场景一致，也存在 复制粘贴 删除多个单元格 v m =null 的场景，都需要做区分
+ * @param data
+ * @returns
+ */
 async function rv(data: string) {
 	/**
 	 * 范围单元格刷新
 	 * 需要先取 range 范围行列数，v 的内容是根据 range 循环而来
 	 */
+	// {"t":"rv","i":"2b62e1f2-7f7f-4889-b34d-007fe7277364",
+	// "v":[
+	// 	[
+	// 		{"ct":{"fa":"General","t":"n"},"bg":"#FFFFFF","ff":"5","fc":"#000000","bl":false,"it":false,"fs":10,"cl":false,"ht":0,"vt":0,"f":null,"un":false}
+	// 	],
+	// 	[
+	// 		{"ct":{"fa":"General","t":"n"},"bg":"#FFFFFF","ff":"5","fc":"#000000","bl":false,"it":false,"fs":10,"cl":false,"ht":0,"vt":0,"f":null,"un":false}
+	// 	]
+	// ],"range":{"row":[0,1],"column":[0,0]}}
 	const { t, i, v, range } = <CRDTDataType<RV>>JSON.parse(data);
 	if (t !== "rv") return logger.error("t is not rv.");
 	if (isEmpty(i)) return logger.error("i is undefined.");
@@ -195,50 +208,58 @@ async function rv(data: string) {
 	logger.info("[CRDT DATA]:", data);
 
 	// 循环列，取 v 的内容，然后创建记录
-	for (let index = 0; index < v.length; index++) {
+	for (let k = 0; k < v.length; k++) {
 		// 这里面的每一项，都是一条记录
-		for (let j = 0; j < v[index].length; j++) {
+		for (let j = 0; j < v[k].length; j++) {
 			// 解析内部的 r c 值
-			const item = v[index][j];
-			const r = range.row[0] + index;
+			const item = v[k][j];
+			const r = range.row[0] + k;
 			const c = range.column[0] + j;
-			// i r c 先判断是否存在记录，存在则更新，不存在则创建
-			const exist = await CellDataService.hasCellData(i, r, c);
 
-			// 检查 item 是否为 null 或 undefined
-			const cellInfo = {
-				worker_sheet_id: i,
-				r,
-				c,
-				f: item?.f || "",
-				ctfa: item?.ct?.fa,
-				ctt: item?.ct?.t,
-				v: <string>item?.v || "",
-				m: <string>item?.m || "",
-				bg: item?.bg,
-				bl: <boolean>item?.bl,
-				cl: <boolean>item?.cl,
-				fc: item?.fc,
-				ff: <string>item?.ff,
-				fs: <number>item?.fs,
-				ht: item?.ht,
-				it: <boolean>item?.it,
-				un: <boolean>item?.un,
-				vt: item?.vt,
-			};
+			// 场景一：设置空单元格的样式数据 加粗、背景、颜色、字号等
+			if (item && item.v === null) {
+				// i r c 先判断是否存在记录，存在则更新，不存在则创建
+				const exist = await CellDataService.hasCellData(i, r, c);
 
-			if (exist) {
-				// 如果存在则更新 - 注意全量的样式数据
-				await CellDataService.updateCellData({
-					cell_data_id: exist.cell_data_id,
-					...cellInfo,
-					bg: cellInfo.bg,
-					bl: <boolean>cellInfo.bl,
-					cl: <boolean>cellInfo.cl,
-					fc: cellInfo.fc,
-					ff: <string>cellInfo.ff,
-				});
-			} else await CellDataService.createCellData(cellInfo);
+				// 检查 item 是否为 null 或 undefined
+				const cellInfo = {
+					worker_sheet_id: i,
+					r,
+					c,
+					f: item?.f || "",
+					ctfa: item?.ct?.fa,
+					ctt: item?.ct?.t,
+					v: <string>item?.v || "",
+					m: <string>item?.m || "",
+					bg: item?.bg,
+					bl: <boolean>item?.bl,
+					cl: <boolean>item?.cl,
+					fc: item?.fc,
+					ff: <string>item?.ff,
+					fs: <number>item?.fs,
+					ht: item?.ht,
+					it: <boolean>item?.it,
+					un: <boolean>item?.un,
+					vt: item?.vt,
+				};
+
+				if (exist) {
+					// 如果存在则更新 - 注意全量的样式数据
+					await CellDataService.updateCellData({
+						cell_data_id: exist.cell_data_id,
+						...cellInfo,
+						bg: cellInfo.bg,
+						bl: <boolean>cellInfo.bl,
+						cl: <boolean>cellInfo.cl,
+						fc: cellInfo.fc,
+						ff: <string>cellInfo.ff,
+					});
+				} else await CellDataService.createCellData(cellInfo);
+			}
+
+			// 场景二：删除单元格内容
+			else if (item && !item.v && !item.m)
+				await CellDataService.deleteCellData(i, r, c); // 删除记录
 		}
 	}
 }
