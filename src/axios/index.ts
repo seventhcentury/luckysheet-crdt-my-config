707d66ec53a6b39a106739b5c65c5326c24fae92
weifegn <1982392655@qq.com>
2025-02-18 11:00:29 +0800

    fix:优化相关代码

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,41 +1,19 @@
-// 封装用户请求axios
-import axios, { AxiosRequestConfig } from "axios";
+import { fetch } from "./core";
 
-export const fetch = (options: AxiosRequestConfig) => {
-	return axios({
-		...options,
-	});
-};
-
-// 添加请求拦截器
-axios.interceptors.request.use(
-	(config) => {
-		// 在发送请求之前进行操作
-		return config;
-	},
-	(error) => {
-		// 对请求错误进行操作
-		return Promise.reject(error);
-	}
-);
-
-// 添加响应拦截器
-axios.interceptors.response.use(
-	function (res) {
-		// 解构返回
-		return res;
-	},
-	function (error) {
-		// 对响应错误进行操作
-		return Promise.reject(error);
-	}
-);
-
-// 导出 API
+// 导出初始化工作簿 API
 export const API_getWorkerBook = (gridKey: string) => {
 	return fetch({
 		url: "/api/getWorkerBook",
 		method: "post",
 		data: { gridKey },
+	});
+};
+
+// 导出图片上传API
+export const API_uploadImage = (data: FormData) => {
+	return fetch({
+		url: "/api/uploadImage",
+		method: "POST",
+		data,
 	});
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,14 @@
-import { fetch } from "../axios";
 import { SERVER_URL } from "../config";
+import { API_uploadImage } from "../axios";
 
 // 处理协同图片上传
 export const uploadImage = async (file: File) => {
 	// 此处拿到的是上传的 file 对象，进行文件上传 ，配合 node 接口实现
 	const formData = new FormData();
 	formData.append("image", file);
-	const { data } = await fetch({
-		url: "/api/uploadImage",
-		method: "POST",
-		data: formData,
-	});
+
+	const { data } = await API_uploadImage(formData);
+
 	// *** 关键步骤：需要返回一个地址给 luckysheet ，用于显示图片
 	if (data.code === 200) return Promise.resolve(data.url);
 	else return Promise.resolve("image upload error");
@@ -19,9 +17,7 @@ export const uploadImage = async (file: File) => {
 // 处理上传图片的地址
 export const imageUrlHandle = (url: string) => {
 	// 已经是 // http data 开头则不处理
-	if (/^(?:\/\/|(?:http|https|data):)/i.test(url)) {
-		return url;
-	}
+	if (/^(?:\/\/|(?:http|https|data):)/i.test(url)) return url;
 	// 不然拼接服务器路径
 	return SERVER_URL + url;
 };
